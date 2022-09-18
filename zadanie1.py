c49e46095f8d712f978e04a90e31b29ac6cb7cbf
Moneno <zaymanov.bulat@mail.ru>
2022-09-18 12:56:24 +0300

    created and complited

---
+++ zadanie1.py
@@ -0,0 +1,12 @@
+chislo = 10
+slovo = "Apple"
+stroka = "Hello from Russia"
+spisok = [1, "one", 43, "two"]
+kortej = ("first", 230, "hi", 55, 230)
+
+print(pow(chislo, 2))
+print(len(slovo))
+print(stroka.upper())
+spisok.reverse()
+print(spisok)
+print(kortej.count(230))
---
+++ zadanie2.py
@@ -0,0 +1,59 @@
+from math import log
+from decimal import Decimal, ROUND_DOWN, ROUND_UP
+
+
+def is_number(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+
+def toNumber(str):
+    if ('.' in str):
+        return float(str)
+    else:
+        return int(str)
+
+
+operations = ['Сложение', 'Вычитание', 'Умножение', 'Деление', 'Возведение в степень', 'Логарифм',
+              'Округление в большую сторону до N знака после запятой', 'Округление в меньшую сторону до N знака после запятой']
+
+oper = input(f'Введите необходимую функцию ({", ".join(operations)}):\n')
+
+while (not (oper in operations)):
+    print('Введите доступную функцию из списка:')
+    oper = input('')
+
+a = input('Введите первый элемент:\n')
+while (is_number(a) == False):
+    a = input('Введенный элемент не является числом, введите первый элемент:\n')
+
+b = input('Введите второй элемент:\n')
+while (is_number(b) == False):
+    b = input('Введенный элемент не является числом, введите второй элемент:\n')
+
+a, b = toNumber(a), toNumber(b)
+
+
+result = 'Результат программы: '
+
+if (oper == operations[0]):
+    result += str(a+b)
+elif (oper == operations[1]):
+    result += str(a-b)
+elif (oper == operations[2]):
+    result += str(a*b)
+elif (oper == operations[3]):
+    result += str(a/b)
+elif (oper == operations[4]):
+    result += str(a**b)
+elif (oper == operations[5]):
+    result += str(log(a, b))
+elif (oper == operations[6]):
+    result += str(Decimal(a).quantize(Decimal(f'1.{"0"*b}'), ROUND_UP))
+else:
+    result += str(Decimal(a).quantize(Decimal(f'1.{"0"*b}'), ROUND_DOWN))
+
+print(result)
