4b822ba30bf348d3e1fc2d3ebc012b0b5a3f44d1
Moneno <zaymanov.bulat@mail.ru>
2022-09-21 09:55:42 +0300

    complited

--- zadanie3.py
+++ zadanie3.py
@@ -1,22 +1,10 @@
 while (True):
-    i = int(input())
-    prostoe = False
+    i = int(input('Введите число:\n'))
     fibonachi = False
     if (i == 1):
-        prostoe = True
         fibonachi = True
     elif (i == 0):
         fibonachi = True
-    k = 0
-    for l in range(2, i//2+1):
-        if (prostoe == True):
-            break
-        if (i % l == 0):
-            k += 1
-    if (k <= 0 and i != 0):
-        prostoe = True
-    else:
-        prostoe = False
     f1 = f2 = 1
     k = i
     while k > 0:
@@ -24,4 +12,4 @@ while (True):
         k -= 1
         if (f2 == i):
             fibonachi = True
-    print('Фибоначчи: ' + str(fibonachi) + ' Простое: ' + str(prostoe))
+    print('Фибоначчи: ' + str(fibonachi))
